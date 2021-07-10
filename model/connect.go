801c32f2eda536e59944a52df4383503433078c1
rslewenstein <rafasoaresjf@gmail.com>
2021-07-10 08:37:57 -0300

    alterando a conexão

--- model/connect.go
+++ model/connect.go
@@ -11,7 +11,7 @@ import (
 var con *sql.DB
 
 func Connect() *sql.DB {
-	db, err := sql.Open("mysql", "root:@123456789@/PeladeirosFut(tcp:localhost:3606)")//user:password@/dbname
+	db, err := sql.Open("mysql", "dev:@abc123456@/tododb(tcp:localhost:3606)") //user:password@/dbname
 	if err != nil {
 		log.Fatal(err)
 	}
